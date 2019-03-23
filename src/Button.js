a183f2554a95c233785fd80dc3686a3c7873d162
Imran Sayed <imranhsayed@gmail.com>
2019-03-23 22:48:59 +0530

    Add a method to create a button and append  the button to body

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+const makeButton = ( buttonName ) => {
+	const buttonLabel = `Button : ${buttonName}`;
+
+	const button = document.createElement( 'button' );
+	button.innerText = buttonLabel;
+
+	return button
+};
+
+module.exports = makeButton;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // const css = require( './css/style.css' );
 
+import makeButton from './Button';
+
+const button = makeButton( 'Click me' );
+document.body.appendChild( button );
+
 const displayName = () => {
 	console.log( 'Bindex' );
 };
